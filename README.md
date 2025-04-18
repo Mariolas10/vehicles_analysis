67f3820cc083999ad6d40df3744cd84421d2f8a7
Mariolas10 <mariolasrv10@gmail.com>
2025-04-17 19:19:48 -0600

    Mi primera aplicacion con Streamlit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Vehiculos
+
+La aplicación se basa en un conjunto de datos de venta de vehiculos que muestran diversas caracteristicas de los mismos. Esta busca crear un Histograma y un diagrama de dispersión sobre los datos arrojados por el odometro, el cual se encarga de recopilar el total de km recorridos por el vehiculo, el histograma busca comprobar los valores atípicos y ayudar a comprender la distribución de los mismos datos, mientras qeu el diagrama de disperción nos ayudara a determinar la relacion que estos datos tienen entre si.
+La aplicación consta de dos casillas (checkbox) las cuales al seleccionar una o ambas se crearan los dos diagramas para los que realizamos la aplicación. 
---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('C:/Users/mario/Documents/TripletenAD/Proyect7/vehicles_analysis/vehicles_us.csv')
+st.header('Análisis de vehículos')
+hist_button = st.checkbox('Construir histrograma')
+dis_button = st.checkbox('Construir diagrama de dispersión')
+
+if hist_button:
+    st.write('Generando el histograma para el conjunto de datos de anuncios de venta de coches')
+    fig_hist = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+if dis_button:
+    st.write('Generando el diagrama de dispesion')
+    fig_dis = px.scatter(car_data, x='odometer', y="price")
+    st.plotly_chart(fig_dis, use_container_width=True)
+
